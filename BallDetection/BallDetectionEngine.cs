3122afbf6b85564b845afdef24f31eb661f68a8f
Matt Divertie <mdivertie@gmail.com>
2018-10-06 18:26:11 -0500

    Add BallDetection class.

---
+++ BallDetection/BallDetectionEngine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Kinect;
+using LightBuzz.Vitruvius;
+
+
+namespace BallDetection
+{
+    public class BallDetectionEngine
+    {
+
+        private DepthSpacePoint[] _depthPoints = null;
+
+        public CoordinateMapper CoordinateMapper { get; set; }
+
+        public int DepthWidth { get; set; }
+
+        public int DepthHeight { get; set; }
+
+        public event EventHandler<BallDetectionResult> BallDetected;
+
+        public BallDetectionEngine(CoordinateMapper coordinateMapper, int depthWidth, int depthHeight)
+        {
+            CoordinateMapper = coordinateMapper;
+            DepthWidth = depthWidth;
+            DepthHeight = depthHeight;
+
+            _depthPoints = new DepthSpacePoint[depthWidth * depthHeight];
+        }
+
+    }
+}
