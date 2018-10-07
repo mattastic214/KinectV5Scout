85d2142425e036ef391368425fe7e83f7677f6f0
Matt Divertie <mdivertie@gmail.com>
2018-10-07 10:10:21 -0500

    Display tracking data to console.

--- PingPongScout/MainWindow.xaml.cs
+++ PingPongScout/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.ComponentModel;
 using BallDetection;
 
 // Below is from WeightLift Sample:
+using System.Windows.Shapes;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -24,7 +25,9 @@ namespace PingPongScout
     {
 
         #region Members
-        
+
+        private List<Ellipse> _points = new List<Ellipse>();
+
         private KinectSensor _kinectSensor = null;
         private MultiSourceFrameReader _multiSourceFrameReader = null;
         private CoordinateMapper _coordinateMapper = null;
@@ -32,6 +35,8 @@ namespace PingPongScout
         private Body _body = null;
         private IList<Body> _bodyData = null;
         private WriteableBitmap _bitmap = null;
+        private InfraredBitmapGenerator _infraredBitmapGenerator = null;
+        private DepthBitmapGenerator _depthBitmapGenerator = null;
         private ushort[] _depthData = null;
         private ushort[] _infraredData = null;
         private ushort[] _longExposureData = null;
@@ -43,6 +48,9 @@ namespace PingPongScout
 
         delegate void constructorOperationDelegate();
         private constructorOperationDelegate constructorOperation;
+
+        delegate void initializeFrameDataDelegate(int depthWidth, int depthHeight);
+        private initializeFrameDataDelegate initializeFrameData;
 
         delegate void generateBodyDataDelegate();
         private generateBodyDataDelegate generateBodyData;
@@ -57,41 +65,20 @@ namespace PingPongScout
 
             constructorOperation += OpenKinect;
             constructorOperation += InitializeMultiSourceReader;
+
+            initializeFrameData += InitializeBitmap;
+            initializeFrameData += InitializeFrameData;
+            initializeFrameData += InitializeBallDetection;
             
             _kinectSensor = KinectSensor.GetDefault();
 
             if (_kinectSensor != null)
             {
-                this.constructorOperation();
-                // Generic start
-                //_kinectSensor.Open();
-                //_coordinateMapper = _kinectSensor.CoordinateMapper;
-
-                // Setup the multiSourceFrameReader
-                //_multiSourceFrameReader = _kinectSensor
-                //    .OpenMultiSourceFrameReader(FrameSourceTypes.Body | FrameSourceTypes.BodyIndex | FrameSourceTypes.Depth | FrameSourceTypes.Infrared | FrameSourceTypes.LongExposureInfrared);
-                //_multiSourceFrameReader.MultiSourceFrameArrived += MultiSourceFrameArrived;
-
-                // Width and Height from sensor -> to camrera!
                 int depthWidth = _kinectSensor.DepthFrameSource.FrameDescription.Width;
                 int depthHeight = _kinectSensor.DepthFrameSource.FrameDescription.Height;
 
-                // Get a bitmap with width and height.
-                _bitmap = new WriteableBitmap(depthWidth, depthHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
-
-                // Kinect sensor Data of interest, 3 of 5. 
-                _bodyData = new Body[_kinectSensor.BodyFrameSource.BodyCount];
-                _infraredData = new ushort[depthWidth * depthHeight];
-                _depthData = new ushort[depthWidth * depthHeight];
-                _bodyIndexData = new byte[depthWidth * depthHeight];
-                _longExposureData = new ushort[depthWidth * depthHeight];
-
-                // Get the BallDetection class set up.
-                _ballDetectionEngine = new BallDetectionEngine(_coordinateMapper, depthWidth, depthHeight);
-                _ballDetectionEngine.BallDetected += BallDetectEvent;
-
-                // Assign the bitmap to the camera! Of course!!
-                camera.Source = _bitmap;
+                constructorOperation();
+                initializeFrameData(depthWidth, depthHeight);
             }
         }
 
@@ -118,44 +105,103 @@ namespace PingPongScout
 
             if (frame != null )
             {
-                Console.WriteLine("Initialize all Frames from MultiFrame");
 
                 using (var bodyFrame = frame.BodyFrameReference.AcquireFrame())
                 {
                     // Placeholder for refactoring.
                     // Handle all logic in another method. Functional Programming?
-                    //bodyFrame?.Dispose();
 
                     if (bodyFrame != null)
                     {
                         bodyFrame.GetAndRefreshBodyData(_bodyData);
 
                         var bodyIEnum = _bodyData.Where(b => b.IsTracked);
-                        var otVitBodies = bodyIEnum.Select(b => BodyWrapper.Create(b, _coordinateMapper, Visualization.Infrared));
+                        var otVitBodies = bodyIEnum.Select(b => BodyWrapper.Create(b, _coordinateMapper, Visualization.Infrared));                        
                         // Ok, you have the bodies you want. What do you want to do with them now?
+
+                        foreach(BodyWrapper bodyWrapper in otVitBodies)
+                        {
+                            if(bodyWrapper.IsTracked)
+                            {
+                                Console.WriteLine("Got bodyWrapper.");
+                                var joints = bodyWrapper.Joints;
+
+                                //var handTipLeft = joints[JointType.HandTipLeft];
+                                //var handTipRight = joints[JointType.HandTipRight];
+                                //var handLeft = joints[JointType.HandLeft];
+                                //var handRight = joints[JointType.HandRight];
+
+                                //var wristLeft = joints[JointType.WristLeft];
+                                //var wristRight = joints[JointType.WristRight];
+                                //var elbowLeft = joints[JointType.ElbowLeft];
+                                //var elbowRight = joints[JointType.ElbowRight];
+                                //var shoulderLeft = joints[JointType.ShoulderLeft];
+                                //var shoulderRight = joints[JointType.ShoulderRight];
+
+                                //var spineShoulder = joints[JointType.SpineShoulder];
+                                //var spineBase = joints[JointType.SpineBase];
+                                //var spineMid = joints[JointType.SpineMid];
+                                //var neck = joints[JointType.Neck];
+                                //var head = joints[JointType.Head];
+
+                                //var hipLeft = joints[JointType.HipLeft];
+                                //var hipRight = joints[JointType.HipRight];
+
+                                
+
+                                //KinectViewer kinectViewer = new KinectViewer();
+                                //kinectViewer.CoordinateMapper = _coordinateMapper;
+                                //kinectViewer.Image;
+                                //kinectViewer.Visualization = Visualization.Infrared;
+                                //kinectViewer.DrawBody(bodyWrapper);
+                                //kinectViewer.InitializeComponent();
+
+                                var jointsAll = joints.Values;
+
+                                var trackedJoints = jointsAll.Where(j => j.TrackingState != TrackingState.NotTracked);
+                                
+                                // Using Infrared, not depth hmmmm....
+                                foreach(Joint joint in trackedJoints)
+                                {
+                                    Console.WriteLine("Position of " + joint.JointType.ToString() + ": X=" + joint.Position.X + " Y=" + joint.Position.Y + " Z=" + joint.Position.Z);
+                                }
+                            }                            
+                        }
                     }
-
-
                 }
+
+
 
                 using (var bodyIndexFrame = frame.BodyIndexFrameReference.AcquireFrame())
-                {
-
-                }
-
                 using (var depthFrame = frame.DepthFrameReference.AcquireFrame())
                 {
+                    if (depthFrame != null && bodyIndexFrame != null)
+                    {
+                        _depthBitmapGenerator.Update(depthFrame, bodyIndexFrame);
+                        
+                        // Pick one.
+                        _bitmap = depthFrame.ToBitmap(bodyIndexFrame);
+                        var bitmapDepth = depthFrame.ToBitmap(bodyIndexFrame);
+                        Console.WriteLine("Depth and BodyIndex Present");
+                    }
 
                 }
-
+                
                 using (var infraredFrame = frame.InfraredFrameReference.AcquireFrame())
                 {
+                    // A job for Async?
 
+                    if (infraredFrame != null)
+                    {
+                        Console.WriteLine("Infrared Present");
+                        _infraredBitmapGenerator.Update(infraredFrame);
+                        var bitmapInfrared = infraredFrame.ToBitmap();
+                    }                    
                 }
 
                 using (var longExposureFrame = frame.LongExposureInfraredFrameReference.AcquireFrame())
                 {
-
+                        
                 }                               
             }
 
@@ -179,6 +225,32 @@ namespace PingPongScout
             _multiSourceFrameReader = _kinectSensor
                     .OpenMultiSourceFrameReader(FrameSourceTypes.Body | FrameSourceTypes.BodyIndex | FrameSourceTypes.Depth | FrameSourceTypes.Infrared | FrameSourceTypes.LongExposureInfrared);
             _multiSourceFrameReader.MultiSourceFrameArrived += MultiSourceFrameArrived;
+        }
+
+        private void InitializeBitmap(int depthWidth, int depthHeight)
+        {
+            _bitmap = new WriteableBitmap(depthWidth, depthHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+            camera.Source = _bitmap;
+
+            _infraredBitmapGenerator = new InfraredBitmapGenerator();
+            _depthBitmapGenerator = new DepthBitmapGenerator();
+        }
+
+        private void InitializeFrameData(int depthWidth, int depthHeight)
+        {
+            int depthArea = depthWidth * depthHeight;
+
+            _bodyData = new Body[_kinectSensor.BodyFrameSource.BodyCount];
+            _infraredData = new ushort[depthArea];
+            _depthData = new ushort[depthArea];
+            _bodyIndexData = new byte[depthArea];
+            _longExposureData = new ushort[depthArea];
+        }
+
+        private void InitializeBallDetection(int depthWidth, int depthHeight)
+        {
+            _ballDetectionEngine = new BallDetectionEngine(_coordinateMapper, depthWidth, depthHeight);
+            _ballDetectionEngine.BallDetected += BallDetectEvent;
         }
     }
 }
